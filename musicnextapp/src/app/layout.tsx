1849d44d9ca99899de7f3d749c7f9a8c9daabcec
Bishal Bhattacharjee <bishalbhattacharjee55@gmail.com>
2024-05-13 09:31:32 +0530

    ok

--- musicnextapp/src/app/layout.tsx
+++ musicnextapp/src/app/layout.tsx
@@ -1,6 +1,8 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="relative w-full flex items-center justify-center ">
+        <Navbar/>
+        </div>
+        {children}</body>
     </html>
   );
 }
